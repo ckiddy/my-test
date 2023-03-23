ddd6b7418999a5c4fbc3a043cb45bc5e7f7f87be
ckiddy <smallckiddy@gmail.com>
2023-03-23 18:27:07 +0530

    source file

---
+++ bigc/${{ values.filePrefix }}App.java
@@ -0,0 +1,16 @@
+package ${{values.package}};
+
+import ${{values.package}}.resources.${{values.filePrefix}}Resource;
+import ${{values.package}}.${{values.filePrefix}}Config;
+
+public class ${filePrefix}App extends RestApplication<${{values.filePrefix}}Config> {
+
+	public static void main(String[] args) throws Exception {
+		new ${{values.filePrefix}}App().run(args);
+	}
+
+	@Override
+	public void start(${{values.filePrefix}}Config t, AppEnvironment environment) throws Exception {
+		environment.registerResource(${{values.filePrefix}}Resource.class);
+	}
+}
